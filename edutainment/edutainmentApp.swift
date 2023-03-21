49f98624c4636ea095b367ee0331dd625eb59673
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-21 10:49:22 +0100

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Hossein Sharifi on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
